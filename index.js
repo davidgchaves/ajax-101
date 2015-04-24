1bf8d774d0e201c2289187b19e6b438d42e062bb
David G Chaves <davidgchaves@gmail.com>
2015-04-24 20:14:18 +0200

    Ready to begin exercise - Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// EXERCISE INSTRUCTIONS:
+//  1 - Attach a button listener to the button
+//  2 - On click on the button, make an AJAX request to reddit for results of cute puppies
+//  3 - http://www.reddit.com/r/aww/search.json?q=puppy&restrict_sr=true
+//  4 - For each of the elements in data.children, append a new <img> element to .text
+//      with the src as the child.data.thumbnail
+
