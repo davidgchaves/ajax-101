264a91760edf4f4c8fca6029e925a58332a9cd44
David G Chaves <davidgchaves@gmail.com>
2015-04-24 20:30:51 +0200

    A working solution
    Remember to
      $ python -m SimpleHTTPServer 8080

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 //  4 - For each of the elements in data.children, append a new <img> element to .text
 //      with the src as the child.data.thumbnail
 
+$('.btn').click(function() {
+  $('.text').text('Working on it...');
+  $.ajax({
+    type: 'GET',
+    data: {
+      q: 'puppy',
+      restrict_sr: 'true',
+    },
+    url: 'http://www.reddit.com/r/aww/search.json',
+    success: function(res) {
+      var i, children, pic;
+      children = res.data.children;
+
+      $('.text').html('');
+      for(i=0; i<children.length; i++) {
+        pic = '<img src="' + children[i].data.thumbnail + '" />';
+        $('.text').append(pic);
+      };
+    },
+  });
+});
+
